eec02de25eb4a83cf9cc090d8f501118e9e32f0c
WELDER NOVAES MIRANDA <wel.miro@hotmail.com>
2020-05-27 17:08:26 -0300

    Created controllers, "main" & "includeConsult" view

--- src/controller/CreateController.java
+++ src/controller/CreateController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class CreateController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class CreateController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private AnchorPane anchorPane2;
+
+    @FXML
+    private JFXButton btnRegister2;
+
+    @FXML
+    void createPatient(MouseEvent event) {
+
+    }
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,71 @@
 package controller;
 
-public class MainController {
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class MainController implements Initializable {
+
+    Logger logger;
+
+    @FXML
+    private JFXButton btnRegisterPatient;
+
+    @FXML
+    private JFXButton btnUpdatePatient;
+
+    @FXML
+    private JFXButton btnDeletePatient;
+
+    @FXML
+    private JFXButton btnCheckPatient;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Pane mainPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void loadCreatePatient(ActionEvent event) {
+        Pane pane;
+        try {
+            pane = FXMLLoader.load(getClass().getResource("/view/layouts/createPatient.fxml"));
+            mainPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            logger.log(Level.ERROR, e);
+        }
+    }
+
+    @FXML
+    void loadDeletePatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadUpdatePatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadCheckPatient(ActionEvent event) {
+
+    }
 
 }
