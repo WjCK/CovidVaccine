389e963e71131c4db782f410bb4fba94cacbe92a
WELDER NOVAES MIRANDA <wel.miro@hotmail.com>
2020-05-30 22:50:10 -0300

    FIXED A LOT OF BUGS

--- src/util/FormException.java
+++ src/util/FormException.java
@@ -5,6 +5,7 @@ import javafx.scene.control.ButtonType;
 
 public class FormException extends Exception {
 
+    private static final long serialVersionUID = 4394296234369178600L;
     final transient Alert alert = new Alert(Alert.AlertType.ERROR, null, ButtonType.CLOSE);
 
     public FormException(String message, String cause) {
